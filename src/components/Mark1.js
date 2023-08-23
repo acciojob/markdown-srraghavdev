1314e2095ff43f21365afda3dda23349ee9c64ee
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-23 14:15:33 +0000

    sdsd

--- src/components/Mark1.js
+++ src/components/Mark1.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
 
 let Mark1= ()=>{
+    let [text,Settext]=useState('')
+    function changetext(event){
+        Settext(event.target.value)
+    }
     return (
         <div className='mark1'>
             <div className='preview'>
-            <h1><strong>Heading</strong></h1>    
-             <textarea>
+            <h1><strong>{text}</strong></h1>    
+             <textarea onChange={changetext}>
             </textarea>   
             </div>
         </div>
